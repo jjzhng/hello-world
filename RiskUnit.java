5341b9906f38e0241dfff1b7f46f40658941f44c
Joy Zhang <9067014+jjzhng@users.noreply.github.com>
2024-11-18 04:25:25 -0500

    Update RiskUnit.java

--- RiskUnit.java
+++ RiskUnit.java
@@ -1,10 +1,37 @@
-import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.*;
 
 public class RiskUnit {
 
+    private String id;
+    private String publicId;
     private String extAgeCode;
+    private String extAnnualMileage;
+    private String extCompSymbol;
     private String extGarageZip;
+    private String extMerit;
+    private String extOtherRating;
+    private String extRatedCity;
     private String extRatedTerritory;
+    private String extRating;
+    private String ruNumber;
+
+    @XmlAttribute(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @XmlAttribute(name = "public-id")
+    public String getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(String publicId) {
+        this.publicId = publicId;
+    }
 
     @XmlElement(name = "ext_AgeCode")
     public String getExtAgeCode() {
@@ -13,6 +40,24 @@ public class RiskUnit {
 
     public void setExtAgeCode(String extAgeCode) {
         this.extAgeCode = extAgeCode;
+    }
+
+    @XmlElement(name = "ext_AnnualMileage")
+    public String getExtAnnualMileage() {
+        return extAnnualMileage;
+    }
+
+    public void setExtAnnualMileage(String extAnnualMileage) {
+        this.extAnnualMileage = extAnnualMileage;
+    }
+
+    @XmlElement(name = "ext_CompSymbol")
+    public String getExtCompSymbol() {
+        return extCompSymbol;
+    }
+
+    public void setExtCompSymbol(String extCompSymbol) {
+        this.extCompSymbol = extCompSymbol;
     }
 
     @XmlElement(name = "ext_GarageZip")
@@ -24,6 +69,33 @@ public class RiskUnit {
         this.extGarageZip = extGarageZip;
     }
 
+    @XmlElement(name = "ext_Merit")
+    public String getExtMerit() {
+        return extMerit;
+    }
+
+    public void setExtMerit(String extMerit) {
+        this.extMerit = extMerit;
+    }
+
+    @XmlElement(name = "ext_OtherRating")
+    public String getExtOtherRating() {
+        return extOtherRating;
+    }
+
+    public void setExtOtherRating(String extOtherRating) {
+        this.extOtherRating = extOtherRating;
+    }
+
+    @XmlElement(name = "ext_RatedCity")
+    public String getExtRatedCity() {
+        return extRatedCity;
+    }
+
+    public void setExtRatedCity(String extRatedCity) {
+        this.extRatedCity = extRatedCity;
+    }
+
     @XmlElement(name = "ext_RatedTerritory")
     public String getExtRatedTerritory() {
         return extRatedTerritory;
@@ -31,5 +103,23 @@ public class RiskUnit {
 
     public void setExtRatedTerritory(String extRatedTerritory) {
         this.extRatedTerritory = extRatedTerritory;
+    }
+
+    @XmlElement(name = "ext_Rating")
+    public String getExtRating() {
+        return extRating;
+    }
+
+    public void setExtRating(String extRating) {
+        this.extRating = extRating;
+    }
+
+    @XmlElement(name = "RUNumber")
+    public String getRuNumber() {
+        return ruNumber;
+    }
+
+    public void setRuNumber(String ruNumber) {
+        this.ruNumber = ruNumber;
     }
 }
