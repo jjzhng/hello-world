c4802569b0bd86a9b58ef615a6ed4f7cf3fd2d20
Joy Zhang <9067014+jjzhng@users.noreply.github.com>
2024-11-18 02:24:32 -0500

    Create Type.java

---
+++ Type.java
@@ -0,0 +1,25 @@
+import javax.xml.bind.annotation.XmlAttribute;
+
+public class Type {
+
+    private String label;
+    private String code;
+
+    @XmlAttribute(name = "label")
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @XmlValue
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
