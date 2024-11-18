76fdb624b777ff934adf4fb0cbabff7aa6a023cd
Joy Zhang <9067014+jjzhng@users.noreply.github.com>
2024-11-18 04:30:22 -0500

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,11 +1,71 @@
-import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.*;
 
 public class Vehicle {
 
+    private String id;
+    private String publicId;
+    private String extBoatLength;
+    private String extBoatVesselType;
+    private LabeledValue extCRISVehicleTypeCode; // Handle `label` attribute and value
+    private String extPremiumTownCd;
     private String make;
     private String model;
     private String vin;
-    private int year;
+    private String year;
+
+    @XmlAttribute(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @XmlAttribute(name = "public-id")
+    public String getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(String publicId) {
+        this.publicId = publicId;
+    }
+
+    @XmlElement(name = "ext_BoatLength")
+    public String getExtBoatLength() {
+        return extBoatLength;
+    }
+
+    public void setExtBoatLength(String extBoatLength) {
+        this.extBoatLength = extBoatLength;
+    }
+
+    @XmlElement(name = "ext_BoatVesselType")
+    public String getExtBoatVesselType() {
+        return extBoatVesselType;
+    }
+
+    public void setExtBoatVesselType(String extBoatVesselType) {
+        this.extBoatVesselType = extBoatVesselType;
+    }
+
+    @XmlElement(name = "ext_CRISVehicleTypeCode")
+    public LabeledValue getExtCRISVehicleTypeCode() {
+        return extCRISVehicleTypeCode;
+    }
+
+    public void setExtCRISVehicleTypeCode(LabeledValue extCRISVehicleTypeCode) {
+        this.extCRISVehicleTypeCode = extCRISVehicleTypeCode;
+    }
+
+    @XmlElement(name = "ext_PremiumTownCd")
+    public String getExtPremiumTownCd() {
+        return extPremiumTownCd;
+    }
+
+    public void setExtPremiumTownCd(String extPremiumTownCd) {
+        this.extPremiumTownCd = extPremiumTownCd;
+    }
 
     @XmlElement(name = "Make")
     public String getMake() {
@@ -35,11 +95,11 @@ public class Vehicle {
     }
 
     @XmlElement(name = "Year")
-    public int getYear() {
+    public String getYear() {
         return year;
     }
 
-    public void setYear(int year) {
+    public void setYear(String year) {
         this.year = year;
     }
 }
